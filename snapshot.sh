6df382a2c3affcd894441e6ee07a4112b6cf0b8b
rouzwelt <rouzweltt@gmail.com>
2026-06-16 20:54:23 +0000

    update

--- snapshot.sh
+++ snapshot.sh
@@ -25,22 +25,22 @@ fi
 # download the pruned snapshot using aria2
 echo "Step 1: downloading snapshot..."
 apt install -y aria2
-aria2c -c -x 16 -s 16 -d $SNAPSHOT_DL_DIR "https://mainnet-reth-$SNAPSHOT_TYPE-snapshots.base.org/$LATEST_SNAPSHOT"
+aria2c -c -x 16 -s 16 -d "$SNAPSHOT_DL_DIR" "https://mainnet-reth-$SNAPSHOT_TYPE-snapshots.base.org/$LATEST_SNAPSHOT"
 echo "Snapshot downloaded to $SNAPSHOT_DL_DIR"
 
 sleep 20
 
 # unpack the snapshot
 echo "Step 2: extracting archive..."
-tar -I zstd -xvf $SNAPSHOT_DL_DIR/$LATEST_SNAPSHOT -C $SNAPSHOT_EXT_DIR
+tar -I zstd -xvf "$SNAPSHOT_DL_DIR/$LATEST_SNAPSHOT" -C "$SNAPSHOT_EXT_DIR"
 echo "Snapshot extracted to $SNAPSHOT_EXT_DIR"
 
 sleep 20
 
 # move snapshot data to host data dir
 echo "Step 3: moving files..."
-mkdir -p -m 775 $HOST_DATA_DIR
-mv $SNAPSHOT_EXT_DIR/snapshots/mainnet/download/* $HOST_DATA_DIR
+mkdir -m 775 "$HOST_DATA_DIR"
+mv "$SNAPSHOT_EXT_DIR"/snapshots/mainnet/download/* "$HOST_DATA_DIR"
 echo "Snapshot moved to $HOST_DATA_DIR"
 
 sleep 20
